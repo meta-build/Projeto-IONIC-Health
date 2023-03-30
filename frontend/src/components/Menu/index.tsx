b418b0dd3b9f2bc524ff1ff3fd2880cd4a341c10
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-03-30 11:21:42 -0300

    Componente Menu + imagem background

---
+++ frontend/src/components/Menu/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Menu.module.scss';
+
+export default function Menu () {
+    return(
+        <nav className={styles.container}>
+            <img src='https://uploads-ssl.webflow.com/60dcc4691817e11aa93685ab/636cfbef568f863947dd4951_logo-color.svg' />
+        </nav>
+    );
+}
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
+import { faGrinHearts } from "@fortawesome/free-regular-svg-icons";
+import IconButton from "../../components/Botao/BotaoDropdown";
 import DropdownPopup from "../../components/DropdownPopup";
+import AddButton from "../../components/Botao/BotaoIcon";
+import Menu from "../../components/Menu";
 
 
 export default function Home () {
     return(
         <div>
-            <DropdownPopup itens={['op1',  'op2', 'op3']} handleSelected={(selected: string) => console.log(selected)}></DropdownPopup>
+            <Menu />
         </div>
-    )
+    );
 }
