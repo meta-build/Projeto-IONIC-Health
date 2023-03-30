cc0e22e0490934d0244a2d6bb399ed53ebdc4753
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-03-29 23:24:22 -0300

    Componente Dropdown Popup

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,10 @@
+import DropdownPopup from "../../components/DropdownPopup";
+
+
 export default function Home () {
     return(
         <div>
+            <DropdownPopup itens={['op1',  'op2', 'op3']} handleSelected={(selected: string) => console.log(selected)}></DropdownPopup>
         </div>
     )
 }
