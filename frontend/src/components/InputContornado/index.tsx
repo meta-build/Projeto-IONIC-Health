cfe0e28d316c31a7175aab6332ca664f4ef4afc7
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-01 10:36:22 -0300

    Componente InputContornado

---
+++ frontend/src/components/InputContornado/index.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import styles from './InputContornado.module.scss'
+
+interface Props {
+    icon?: JSX.Element;
+    placeholder?: string;
+    className?: string | any;
+    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function InputContornado (props: Props) {
+    return(
+        <label className={classNames({
+            [styles.label]: true,
+            [props.className]: true
+        })}>
+            <span className={styles.icon}>
+                {props.icon && props.icon}
+            </span>
+            <input
+            className={styles.input}
+            onChange={props.handleChange}
+            placeholder={props.placeholder} />
+        </label>
+    );
+}
--- frontend/src/pages/Tests/index.tsx
+++ frontend/src/pages/Tests/index.tsx
@@ -1,20 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAddressBook, faAngry } from '@fortawesome/free-regular-svg-icons'
-import DropdownContornado from "../../components/DropdownContornado";
-import GoogleIcon from "../../components/GoogleIcon";
-import DropdownItem from "../../types/DropdownItem";
+import { faAddressBook } from '@fortawesome/free-regular-svg-icons'
+import InputContornado from "../../components/InputContornado";
 
 export default function Tests () {
     return (
         <>
-            <DropdownContornado
-            itens={[
-                new DropdownItem('Feature', <GoogleIcon>&#xE8B8;</GoogleIcon>),
-                new DropdownItem('Hotfix', <GoogleIcon>&#xf10b;</GoogleIcon>)
-            ]}
-            handleSelected={(s: string) => console.log(s)}
-            icon={<GoogleIcon>&#xe89c;</GoogleIcon>}
-            />
+            <InputContornado
+            icon={<FontAwesomeIcon icon={faAddressBook} />}
+            placeholder="teste"
+            handleChange={(e) => console.log(e)} />
         </>
     )
 }
