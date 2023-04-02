280806cd11b780e110ef84ad101b35ce8b0f080d
fChicoNeto <francisco14042000@gmail.com>
2023-04-02 13:02:03 -0300

    initial method find all Solicitacao
    criado a função simples para retornar tudo do banco de dados via metodo get

--- backend/src/services/GetAllSolicitacao.ts
+++ backend/src/services/GetAllSolicitacao.ts
@@ -4,13 +4,12 @@ import { User } from '../entities/User';
 
 class GetAllSolicitacao {
 
-public async getAllSolicitacao(req: Request, res: Response) { 
-    const { nomeSolicitacao, tipoSolicitacao, solicitante } = req.params;
-    return res.json
-   
-  }
+public async getAllSolicitacao(req: Request, res: Response){
+    const usuario: any = await AppDataSource
+    .getRepository(User)
+    .find()
+    console.log(usuario)
+    return usuario
 }
-
-
-
+    }
 export default new GetAllSolicitacao();
