63419fb284a427fd8259e85be2057af0bec0f9f2
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-02 20:23:21 -0300

    Componente AcaoNotas

---
+++ frontend/src/components/ItemLista/ItemAcoes/AcaoNotas/index.tsx
@@ -0,0 +1,54 @@
+import BotaoNota from "../../../BotaoNota";
+import styles from './AcaoNotas.module.scss';
+
+interface Props {
+    notaRisco: number;
+    notaImpacto: number;
+    notaCusto: number;
+    notaPreenchida?: boolean
+}
+
+export default function AcaoNotas (props: Props) {
+
+    const {notaPreenchida = false} = props;
+
+    const corRiscoCusto:
+    ("cinza" | "verde" | "amarelo" | "vermelho" | "azul1" | "azul2")[] =
+    [ 'cinza', 'verde', 'amarelo', 'vermelho' ]
+
+    const corImpacto:
+    ("cinza" | "verde" | "amarelo" | "vermelho" | "azul1" | "azul2")[] =
+    [ 'cinza', 'verde', 'azul1', 'azul2' ]
+
+    return (
+        <span className={styles.container}>
+            <span className={styles.nota}>
+                Risco:
+                <BotaoNota
+                valor={props.notaRisco}
+                cor={corRiscoCusto[props.notaRisco]}
+                selecionado={notaPreenchida}
+                className={styles.numero}
+                />
+            </span>
+            <span className={styles.nota}>
+                Impacto:
+                <BotaoNota
+                valor={props.notaImpacto}
+                cor={corImpacto[props.notaImpacto]}
+                selecionado={notaPreenchida}
+                className={styles.numero}
+                />
+            </span>
+            <span className={styles.nota}>
+                Custo:
+                <BotaoNota
+                valor={props.notaCusto}
+                cor={corRiscoCusto[props.notaCusto]}
+                selecionado={notaPreenchida}
+                className={styles.numero}
+                />
+            </span>
+        </span>
+    );
+}
