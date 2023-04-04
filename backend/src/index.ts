502f6fd2afb87a0f8a586ef2f2d3a54818e40185
fNetoCardoso <francisco14042000@gmail.com>
2023-04-04 07:16:44 -0300

    adding cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as dotenv from 'dotenv';
+import *  as x from 'cors'
 dotenv.config();
 
 import routes from './routes';
@@ -7,7 +8,16 @@ import routes from './routes';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(x)
 app.use(express.json());
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: x.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(x(options))
 
 app.use(routes);
