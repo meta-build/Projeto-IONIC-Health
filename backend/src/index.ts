bbbd5dfcd647a56190849676e2c9578ce3583188
fNetoCardoso <francisco14042000@gmail.com>
2023-04-04 07:30:41 -0300

    removing cors
    cors errado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,5 @@
 import * as express from "express";
 import * as dotenv from 'dotenv';
-import *  as x from 'cors'
 dotenv.config();
 
 import routes from './routes';
@@ -8,16 +7,9 @@ import routes from './routes';
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(x)
-app.use(express.json());
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+app.use(express.json());
 
-const allowedOrigins = ['http://localhost:3000'];
 
-const options: x.CorsOptions = {
-  origin: allowedOrigins
-};
-
-app.use(x(options))
 
 app.use(routes);
