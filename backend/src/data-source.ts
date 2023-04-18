c1801f92ca793bd20c9faeea9d4b8bc89299d823
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-18 18:52:01 -0300

    chore: update DataSource to use environment variables

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,19 +1,20 @@
 import { DataSource } from "typeorm";
+import env from './main/config/env';
 
 //https://orkhan.gitbook.io/typeorm/docs/data-source-options
 const AppDataSource = new DataSource({
-    database: 'bd_ionic', // se for SQLite, então use bdaula.db
-    type: "mysql", // se for SQLite, então use sqlite
-    host: 'localhost', // não use esta propriedade se for sqlite
-    port: 3306, // não use esta propriedade se for sqlite
-    username: 'rodrigo', // não use esta propriedade se for sqlite
-    password:'fatec', // não use esta propriedade se for sqlite
+    type: 'mysql', // se for SQLite, então use sqlite
+    database: env.database, // se for SQLite, então use bdaula.db
+    host: env.dbHost, // não use esta propriedade se for sqlite
+    port: env.dbPort, // não use esta propriedade se for sqlite
+    username: env.dbUser, // não use esta propriedade se for sqlite
+    password: env.dbPassword, // não use esta propriedade se for sqlite
     // true indica que o schema do BD será criado a cada vez que a aplicação inicializar
     // deixe false ao usar migrations
-    synchronize: true, 
+    synchronize: true,
     logging: false, // true indica que as consultas e erros serão exibidas no terminal
-    entities: ["src/entities/*.ts"], // entidades que serão convertidas em tabelas
-    migrations: ["src/migrations/*.ts"], // local onde estarão os arquivos de migração
+    entities: ['src/entities/*.ts'], // entidades que serão convertidas em tabelas
+    migrations: ['src/migrations/*.ts'], // local onde estarão os arquivos de migração
     subscribers: [],
     maxQueryExecutionTime: 2000 // 2 seg.
 });
@@ -22,10 +23,10 @@ const AppDataSource = new DataSource({
 AppDataSource
     .initialize()
     .then(() => {
-        console.log("Data Source inicializado!")
+        console.log('Data Source inicializado!')
     })
     .catch((e) => {
-        console.error("Erro na inicialização do Data Source:", e)
+        console.error('Erro na inicialização do Data Source:', e)
     });
 
 export default AppDataSource;
