f61bac35e89a2e27cee6ce2851dc75d4ab1254a1
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-19 14:49:40 -0300

    feat: ensure LocalFileStorage implements SaveFile

---
+++ backend/src/infra/storage/multer-file-storage.ts
@@ -0,0 +1,19 @@
+import { SaveFile } from '../../domain/contracts'
+import { FileData } from '../../domain/entities'
+
+import path from 'path'
+import fs from 'fs'
+
+export class LocalFileStorage implements SaveFile {
+  private readonly storagePath = 'uploads/'
+
+  async saveFile (fileData: FileData): Promise<string> {
+    const { buffer, fileName } = fileData
+
+    const filePath = path.join(this.storagePath, fileName)
+
+    await fs.promises.writeFile(filePath, buffer)
+
+    return filePath
+  }
+}
