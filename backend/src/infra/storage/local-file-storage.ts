44f5ed7e2a2537d7ebe0f11a388c5e986c80d4eb
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-19 20:39:29 -0300

    feat: ensure LocalFileStorage creates file correctly

--- backend/src/infra/storage/local-file-storage.ts
+++ backend/src/infra/storage/local-file-storage.ts
@@ -5,15 +5,22 @@ import path from 'path'
 import fs from 'fs'
 
 export class LocalFileStorage implements SaveFile {
-  private readonly storagePath = path.join('../../', 'uploads')
+  private readonly storagePath = path.join(__dirname, '../../../', 'uploads')
+  type = 'local'
 
   async saveFile (fileData: FileData): Promise<string> {
     const { buffer, fileName } = fileData
-
     const filePath = path.join(this.storagePath, fileName)
+
+    try {
+      await fs.promises.mkdir(path.dirname(filePath), { recursive: true })
+    } catch (err) {
+      console.error('Erro ao criar diretório:', err)
+      throw err
+    }
 
     await fs.promises.writeFile(filePath, buffer)
 
-    return filePath
+    return `/uploads/${fileName}`
   }
 }
