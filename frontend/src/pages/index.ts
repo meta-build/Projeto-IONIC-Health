16db43a706464227db5b8d4d14b7aac9abe56b0f
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-18 23:14:14 -0300

    Página Home Admnistrador

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,4 +1,4 @@
-import Home from "./Home";
+import Home from "./HomeAdm";
 import Tests from "./Tests";
 import Login from "./Login";
 import PaginaComHeader from "../components/PaginaComHeader";
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,6 +5,7 @@ import { useContexto } from "./context/contexto";
 import Usuario from "./types/Usuario";
 import HomeSolicitante from "./pages/HomeSolicitante";
 import HomeAvaliador from "./pages/HomeAvaliador";
+import HomeAdm from "./pages/HomeAdm";
 
 export default function AppRouter() {
 
@@ -36,9 +37,11 @@ export default function AppRouter() {
               {usuario.getGrupo() == 'avaliador' && (
                 <Route path='/home' element={<PaginaComHeader elemento={<HomeAvaliador />} />} />
               )}
-              {usuario.getGrupo() == 'adm' && (
-                <Route path='/home' element={<PaginaComHeader elemento={<span>adm</span>} />} />
-              )}
+              {usuario.getGrupo() == 'adm' && (<>
+                  <Route path='/home' element={<PaginaComHeader elemento={<HomeAdm />} />} />
+                  <Route path='/solicitacoes' element={<PaginaComHeader elemento={<span>solicitacoes</span>} />} />
+                  <Route path='/usuarios' element={<PaginaComHeader elemento={<span>usuarios</span>} />} />
+              </>)}
               <Route path='/tests' element={<Tests />} />
             </>  
           )}
