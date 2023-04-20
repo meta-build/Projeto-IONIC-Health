11dbf3866093544a50af524c1459c69b96d674bc
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-19 23:08:25 -0300

    Correção de bug dos dropdowns

--- frontend/src/components/Dropdowns/DropdownPreenchido/index.tsx
+++ frontend/src/components/Dropdowns/DropdownPreenchido/index.tsx
@@ -3,7 +3,6 @@ import classNames from "classnames";
 import { ReactNode, useState } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import DropdownItem from '../../types/DropdownItem';
 
 interface Props {
     itens: string[];
@@ -16,16 +15,11 @@ export default function DropdownPreenchido (props: Props) {
     const [aberto, setAberto] = useState(false);
     const [selecionado, setSelecionado] = useState(props.selecionadoFst || props.itens[0]);
 
-    // itens feature/hotfix
-    // itens={[
-    //     new DropdownItem('Feature', <GoogleIcon>&#xE8B8;</GoogleIcon>),
-    //     new DropdownItem('Hotfix', <GoogleIcon>&#xf10b;</GoogleIcon>)
-    // ]}
-
     return(
         <div className={styles.container}>
             <button
             onClick={() => setAberto(!aberto)}
+            onBlur={() => setAberto(false)}
             className={classNames({
                 [styles.botao]: true
             })}>
@@ -35,7 +29,6 @@ export default function DropdownPreenchido (props: Props) {
                 {aberto ?
                 <FontAwesomeIcon icon={faChevronUp} className={styles.arrow} /> :
                 <FontAwesomeIcon icon={faChevronDown} className={styles.arrow} />}
-            </button>
 
             {aberto && 
             <ul className={styles.list} >
@@ -45,6 +38,7 @@ export default function DropdownPreenchido (props: Props) {
                 </button></li>
             ))}
             </ul>}
+            </button>
         </div>
     );
 }
