f124150ee6beb0b31372d0c23597e5e744bf5535
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-20 11:45:33 -0300

    Implementação das propriedades tipo e onFocus no InputPopup

--- frontend/src/components/Inputs/InputPopup/index.tsx
+++ frontend/src/components/Inputs/InputPopup/index.tsx
@@ -1,26 +1,31 @@
 import classNames from 'classnames';
 import styles from './InputPopup.module.scss';
+import { useState } from 'react';
 
 interface Props {
     icon?: JSX.Element;
     placeholder?: string;
     className?: string | any;
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    valor?: string
+    handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    valor: string;
+    tipo?: React.HTMLInputTypeAttribute;
+    onFocus?: () => void;
 }
-
 export default function InputPopup (props: Props) {
-    const {valor = ''} = props;
-
+    const [tipo, setTipo] = useState(props.tipo ?? 'text');
     return(
-        <input
-        className={classNames({
-            [styles.input]: true,
-            [props.className]: true
-        })}
-        onChange={props.handleChange}
-        placeholder={props.placeholder}
-        value={valor}
-        />
+        <>
+            <input
+            className={classNames({
+                [styles.input]: true,
+                [props.className]: true
+            })}
+            onChange={props.handleChange}
+            onFocus={props.onFocus}
+            placeholder={props.placeholder}
+            value={props.valor}
+            type={tipo}
+            />
+        </>
     );
 }
