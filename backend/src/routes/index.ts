fe900a9b6305c5014cf8ff5577fa3df0ce854b76
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-21 20:41:05 -0300

    feat: add post rating route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,39 +1,53 @@
-import user from "./user";
-import grupo from "./grupo";
-import notificacao from "./notificacao";
-import UserController from "../controllers/UserController";
-import GetOneSolicitacao from "../services/GetOneSolicitacao";
-import GetAllSolicitacao from "../services/GetAllSolicitacao";
-import ArchiveSolicitacao from "../services/ArchiveSolicitacao";
-import deleteSolicitacao from "../services/deleteSolicitacao";
-import SolicitacaoController from "../controllers/SolicitacaoController";
-import GrupoController from "../controllers/GrupoController";
-import { authorization } from "../middlewares";
-import { adaptMulter as upload } from '../main/adapters/multer';
-import { makeTicketController } from '../main/factories/application/controllers';
-import { adaptRoute } from '../main/adapters/express-router';
+import user from './user'
+import grupo from './grupo'
+import notificacao from './notificacao'
+import UserController from '../controllers/UserController'
+import GetOneSolicitacao from '../services/GetOneSolicitacao'
+import GetAllSolicitacao from '../services/GetAllSolicitacao'
+import ArchiveSolicitacao from '../services/ArchiveSolicitacao'
+import deleteSolicitacao from '../services/deleteSolicitacao'
+import SolicitacaoController from '../controllers/SolicitacaoController'
+import GrupoController from '../controllers/GrupoController'
+import { authorization } from '../middlewares'
+import { adaptMulter as upload } from '../main/adapters/multer'
+import {
+  makeCreateRatingController,
+  makeTicketController
+} from '../main/factories/application/controllers'
+import { adaptRoute } from '../main/adapters/express-router'
 
-import { Router } from "express";
+import { Router } from 'express'
 
-const routes = Router();
+const routes = Router()
 
-routes.use("/usuario", user);
-routes.post("/login", UserController.login);
-routes.post("/create/usuario", UserController.create);
-routes.put("/update/usuario/:id", UserController.update);
+routes.use('/usuario', user)
+routes.post('/login', UserController.login)
+routes.post('/create/usuario', UserController.create)
+routes.put('/update/usuario/:id', UserController.update)
 
-routes.use("/notificacao", authorization, notificacao);
+routes.use('/notificacao', authorization, notificacao)
 
-routes.post("/ticket", authorization, upload, adaptRoute(makeTicketController()));
+routes.post(
+  '/ticket',
+  authorization,
+  upload,
+  adaptRoute(makeTicketController())
+)
 
-routes.put("/update/solicitacao/:id", authorization, SolicitacaoController.update);
+routes.put(
+  '/update/solicitacao/:id',
+  authorization,
+  SolicitacaoController.update
+)
 
-routes.get("/solicitacao/:id", GetOneSolicitacao.getSolicitacaoById);
-routes.get("/all", GetAllSolicitacao.getAllSolicitacao);
-routes.put("/arquivo/:id", ArchiveSolicitacao.archiveSolicitacao);
-routes.delete("/delete/:id", deleteSolicitacao.deleteSolicitacao);
+routes.get('/solicitacao/:id', GetOneSolicitacao.getSolicitacaoById)
+routes.get('/all', GetAllSolicitacao.getAllSolicitacao)
+routes.put('/arquivo/:id', ArchiveSolicitacao.archiveSolicitacao)
+routes.delete('/delete/:id', deleteSolicitacao.deleteSolicitacao)
 
-routes.use("/grupo", authorization, grupo);
-routes.post("/create/grupo", authorization, GrupoController.create);
+routes.use('/grupo', authorization, grupo)
+routes.post('/create/grupo', authorization, GrupoController.create)
 
-export default routes;
+routes.post('/rating', authorization, adaptRoute(makeCreateRatingController()))
+
+export default routes
