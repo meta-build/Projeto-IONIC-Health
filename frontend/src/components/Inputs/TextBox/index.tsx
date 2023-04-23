33493d9e51998a4977c558255456013588f5c463
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-22 23:58:56 -0300

    Update TextBox

--- frontend/src/components/Inputs/TextBox/index.tsx
+++ frontend/src/components/Inputs/TextBox/index.tsx
@@ -1,17 +1,26 @@
+import classNames from "classnames";
 import styles from "./TextBox.module.scss";
 
 
 interface Props {
     placeholder?:string;
+    onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+    className?: string;
+    ajustavel?: boolean;
 }
 
 export default function TextBox(props: Props) {
-    
+  const {ajustavel = true} = props;
+
   return ( 
     <textarea 
-        className={styles.textBox}
+        className={classNames({
+          [styles.textBox]: true,
+          [styles['nao-ajustavel']]: !ajustavel,
+          [props.className]: true
+        })}
         placeholder={props.placeholder}
-
+        onChange={props.onChange}
     />
   );
 };
