46de84e5a138ca3222479bf464d07d7eadba86c3
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-23 16:34:13 -0300

    Update visualizar solicitação em produção

--- frontend/src/popUps/VizualizarSolicitacaoProducao/index.tsx
+++ frontend/src/popUps/VizualizarSolicitacaoProducao/index.tsx
@@ -5,25 +5,30 @@ import styles from './VizualizarSolicitacaoProducao.module.scss';
 import BotaoPreenchido from "../../components/Botoes/BotaoPreenchido";
 import { AcaoProducao } from "../../components/ItemLista/Acoes";
 import AlterarStatusProducao from "../AlterarStatusProducao";
+import ConfirmarArquivamentoSolicitacao from "../ConfirmarArquivamentoSolicitacao";
+import ConfirmarExclusaoSolicitacao from "../ConfirmarExclusaoSolicitacao";
 
 interface Props {
   aberto: boolean;
   onClose: () => void;
+  usuario: 'adm' | 'solicitante';
 }
 
 export default function VizualizarSolicitacaoProducao(props: Props) {
   const [titulo, setTitulo] = useState('exemplo');
   const [tipo, setTipo] = useState('Feature');
   const [status, setStatus] = useState<"new" | "on-holding" | "done">('on-holding');
+  const [desc, setDesc] = useState('lorem ipsum');
 
-  const [popup, setPopup] = useState(false);
-  
+  const [popupAlterar, setPopupAlterar] = useState(false);
+  const [popupArquivar, setPopupArquivar] = useState(false);
+  const [popupExclusao, setPopupExclusao] = useState(false);  
+
   return (
     <PopUp
       titulo={`${tipo} ${titulo}`}
       visivel={props.aberto}
       onClose={props.onClose} >
-
       <div className={styles.form}>
         <div className={styles.inputs}>
           <div
@@ -35,7 +40,7 @@ export default function VizualizarSolicitacaoProducao(props: Props) {
             <span className={styles.label}>Descrição</span>
 
             <span className={styles.conteudo}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum libero mauris, a posuere lacus elementum quis. Vestibulum in lorem at nibh semper facilisis a ut augue. Praesent sed magna sed dui condimentum elementum. Donec nec tortor tincidunt urna bibendum semper. Duis sed malesuada ipsum. Nunc ullamcorper sodales libero, a varius metus facilisis sit amet. Praesent ac mi sit amet ligula commodo sollicitudin nec sit amet nibh. Aenean ultricies lorem et ex ullamcorper, vel volutpat odio semper. Praesent efficitur, nisi eu tristique lacinia, enim arcu vestibulum felis, at semper erat urna vitae orci. Duis imperdiet ante non ullamcorper laoreet. Integer luctus sed nisl quis fermentum. In ut nisl nec libero tristique maximus. Suspendisse sagittis nisl at velit laoreet suscipit.
+              {desc}
             </span>
             <span className={styles.label}>Arquivos</span>
             <span className={styles.arquivos}>
@@ -60,16 +65,30 @@ export default function VizualizarSolicitacaoProducao(props: Props) {
                 </span>
             </div>
             <div className={styles['linha-submit']}>
-              {/* verificar usuário e exibir os botões corretos */}
-              <BotaoPreenchido
-              handleClick={() => setPopup(true)}>
-                Alterar status produção
-              </BotaoPreenchido>
+              {props.usuario == 'adm' && <>
+                <BotaoPreenchido
+                className={styles.botao}
+                handleClick={() => setPopupArquivar(true)}>
+                  Arquivar
+                </BotaoPreenchido>
+                <BotaoPreenchido
+                className={styles.botao}
+                handleClick={() => setPopupExclusao(true)}>
+                  Excluir
+                </BotaoPreenchido>
+                <BotaoPreenchido
+                className={styles.botao}
+                handleClick={() => setPopupAlterar(true)}>
+                  Alterar status produção
+                </BotaoPreenchido>
+              </>}
             </div>
           </div>
         </div>
       </div>
-      <AlterarStatusProducao aberto={popup} onClose={() => setPopup(false)} />
+      <AlterarStatusProducao aberto={popupAlterar} onClose={() => setPopupAlterar(false)} />
+      <ConfirmarArquivamentoSolicitacao aberto={popupArquivar} onClose={() => setPopupArquivar(false)} />
+      <ConfirmarExclusaoSolicitacao aberto={popupExclusao} onClose={() => setPopupExclusao(false)} />
     </PopUp>
   )
 }
