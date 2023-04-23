08b69d13d30f14fe4c0c1405f00d9edb388f4b18
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-04-23 16:52:20 -0300

    Update popup avaliar solicitação

--- frontend/src/popUps/AvaliarSolicitacao/index.tsx
+++ frontend/src/popUps/AvaliarSolicitacao/index.tsx
@@ -15,11 +15,15 @@ export default function AvaliarSolicitacao(props: Props) {
     const [nota, setNota] = useState<number>();
     const [comentario, setComentario] = useState('');
 
+    const [titulo, setTitulo] = useState('');
+    const [tipo, setTipo] = useState('');
+    const [desc, setDesc] = useState('');
+
     return (
         <PopUp
             visivel={props.aberto}
             onClose={props.onClose}
-            titulo="Avaliação de Risco da Feature tal">
+            titulo={`Avaliação da ${tipo} ${titulo}`}>
             <form
                 className={styles.form}
                 onSubmit={(e) => {
@@ -33,24 +37,24 @@ export default function AvaliarSolicitacao(props: Props) {
                     })}>
                         <span className={styles.label}>Descrição</span>
                         <span className={styles.conteudo}>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vestibulum libero mauris, a posuere lacus elementum quis. Vestibulum in lorem at nibh semper facilisis a ut augue. Praesent sed magna sed dui condimentum elementum. Donec nec tortor tincidunt urna bibendum semper. Duis sed malesuada ipsum. Nunc ullamcorper sodales libero, a varius metus facilisis sit amet. Praesent ac mi sit amet ligula commodo sollicitudin nec sit amet nibh. Aenean ultricies lorem et ex ullamcorper, vel volutpat odio semper. Praesent efficitur, nisi eu tristique lacinia, enim arcu vestibulum felis, at semper erat urna vitae orci. Duis imperdiet ante non ullamcorper laoreet. Integer luctus sed nisl quis fermentum. In ut nisl nec libero tristique maximus. Suspendisse sagittis nisl at velit laoreet suscipit.
+                            {desc}
                         </span>
-                        <span className={styles.label}>Arquivos</span>
-                        <span className={styles.arquivos}>
-                            <BotaoPreenchido
-                                className={styles.arquivo}
-                                handleClick={() => console.log('foi')}>
-                                arquivo.png
-                            </BotaoPreenchido>
-                        </span>
-                        <div className={styles.subtitulo}>
-                            <div>
-                                Criado em 01/01/2023 por Fulano de tal
-                            </div>
-                            <div>
-                                Editado em 01/01/2023 por Ciclano de tal
-                            </div>
+                    <span className={styles.label}>Arquivos</span>
+                    <span className={styles.arquivos}>
+                        <BotaoPreenchido
+                            className={styles.arquivo}
+                            handleClick={() => console.log('foi')}>
+                            arquivo.png
+                        </BotaoPreenchido>
+                    </span>
+                    <div className={styles.subtitulo}>
+                        <div>
+                            Criado em 01/01/2023 por Fulano de tal
                         </div>
+                        <div>
+                            Editado em 01/01/2023 por Ciclano de tal
+                        </div>
+                    </div>
                     </div>
                 </div>
 
