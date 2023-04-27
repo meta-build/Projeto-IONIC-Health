86d12335ebdafdf7cea13e898938ae7f12374d39
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-27 18:10:40 -0300

    refactor: update app composition

---
+++ backend/src/main/config/app.ts
@@ -0,0 +1,10 @@
+import { setupMiddlewares } from './middlewares'
+import { setupRoutes } from './routes'
+
+import express from 'express'
+
+const app = express()
+setupMiddlewares(app)
+setupRoutes(app)
+
+export default app
