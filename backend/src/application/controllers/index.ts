191c86286ed23d128a6359e62d64b3a53a97c9c7
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-27 19:11:46 -0300

    refactor: update controllers index to include all controllers

--- backend/src/application/controllers/index.ts
+++ backend/src/application/controllers/index.ts
@@ -1,6 +1,14 @@
+export * from './group/GrupoController'
+
+export * from './notification/NotificacaoController'
+
+export * from './rating/createRatingController'
+
 export * from './ticket/createTicketController'
 export * from './ticket/SolicitacaoController'
-export * from './rating/createRatingController'
-export * from './group/GrupoController'
-export * from './notification/NotificacaoController'
+export * from './ticket/ArchiveSolicitacao'
+export * from './ticket/deleteSolicitacao'
+export * from './ticket/getAllSolicitacao'
+export * from './ticket/getOneSolicitacao'
+
 export * from './user/UserController'
