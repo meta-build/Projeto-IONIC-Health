399e001f549e5bb1dbf92510a8af0c60583a3b89
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-27 18:05:37 -0300

    chore: add port to env config

--- backend/src/main/config/env.ts
+++ backend/src/main/config/env.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
+  port: process.env.PORT || 3000,
   database: process.env.DB_DATABASE ?? 'db_ionic',
   dbPort: parseInt(process.env.DB_PORT) ?? 3306,
   dbHost: process.env.DB_HOST ?? 'localhost',
