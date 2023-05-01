3af5de3181606c225eeeb11970e45048ffa13b57
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-30 21:16:12 -0300

    feat: ensure uploads static route includes correct Content-Type

--- backend/src/main/config/routes.ts
+++ backend/src/main/config/routes.ts
@@ -1,11 +1,19 @@
 import express, { Express, Router } from 'express'
 import { readdirSync } from 'fs'
+import mime from 'mime-types'
 import path from 'path'
 
 export const setupRoutes = (app: Express): void => {
   const router = Router()
   const uploadsPath = path.join(__dirname, '../../../', 'uploads')
-  app.use('/uploads', express.static(uploadsPath))
+  app.use('/uploads', express.static(uploadsPath, {
+    setHeaders: (res, path) => {
+      const mimeType = mime.lookup(path)
+      if (mimeType) {
+        res.set('Content-Type', mimeType)
+      }
+    }
+  }))
   app.use('/api', router)
   readdirSync(path.join(__dirname, '..', 'routes')).map(async file => {
     (await import(`../routes/${file}`)).default(router)
