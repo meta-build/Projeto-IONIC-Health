d684ceb87ae6efa47821b89d12c6145f54c42261
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-04-30 21:13:52 -0300

    feat: add ok and badRequest http helpers

--- backend/src/application/helpers/http.ts
+++ backend/src/application/helpers/http.ts
@@ -2,3 +2,13 @@ export type HttpResponse<T = any> = {
   statusCode: number
   data: T
 }
+
+export const ok = <T = any> (data: T): HttpResponse<T> => ({
+  statusCode: 200,
+  data
+})
+
+export const badRequest = (error: Error): HttpResponse<Error> => ({
+  statusCode: 400,
+  data: error
+})
