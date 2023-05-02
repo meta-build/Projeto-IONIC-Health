3ccf32a0880a821cbb04d62a6ee2d617d8115628
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-02 12:07:02 -0300

    style: update code indent in inputs components

--- frontend/src/components/Inputs/InputContornado/index.tsx
+++ frontend/src/components/Inputs/InputContornado/index.tsx
@@ -2,29 +2,29 @@ import classNames from 'classnames';
 import styles from './InputContornado.module.scss'
 
 interface Props {
-    icon?: JSX.Element;
-    placeholder?: string;
-    className?: string | any;
-    tipo?: React.HTMLInputTypeAttribute;
-    handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  icon?: JSX.Element;
+  placeholder?: string;
+  className?: string | any;
+  tipo?: React.HTMLInputTypeAttribute;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function InputContornado (props: Props) {
-    const {tipo = 'text'} = props;
+export default function InputContornado(props: Props) {
+  const { tipo = 'text' } = props;
 
-    return(
-        <label className={classNames({
-            [styles.label]: true,
-            [props.className]: true
-        })}>
-            <span className={styles.icon}>
-                {props.icon && props.icon}
-            </span>
-            <input
-            className={styles.input}
-            onChange={props.handleChange}
-            type={tipo}
-            placeholder={props.placeholder} />
-        </label>
-    );
+  return (
+    <label className={classNames({
+      [styles.label]: true,
+      [props.className]: true
+    })}>
+      <span className={styles.icon}>
+        {props.icon && props.icon}
+      </span>
+      <input
+        className={styles.input}
+        onChange={props.handleChange}
+        type={tipo}
+        placeholder={props.placeholder} />
+    </label>
+  );
 }
--- frontend/src/components/Inputs/TextBox/index.tsx
+++ frontend/src/components/Inputs/TextBox/index.tsx
@@ -3,35 +3,35 @@ import styles from "./TextBox.module.scss";
 import { useEffect, useState } from "react";
 
 interface Props {
-    placeholder?:string;
-    onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
-    className?: string;
-    ajustavel?: boolean;
-    valor?: string;
+  placeholder?: string;
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
+  className?: string;
+  ajustavel?: boolean;
+  valor?: string;
 }
 
 export default function TextBox(props: Props) {
-  const {ajustavel = true} = props;
+  const { ajustavel = true } = props;
   const [valor, setValor] = useState(props.valor);
 
   useEffect(() => {
-    if(props.valor) {
-      setValor(props.valor)
+    if (props.valor) {
+      setValor(props.valor);
     }
   }, [props.valor]);
-  return ( 
-    <textarea 
-        className={classNames({
-          [styles.textBox]: true,
-          [styles['nao-ajustavel']]: !ajustavel,
-          [props.className]: true
-        })}
-        placeholder={props.placeholder}
-        onChange={(e) => {
-          setValor(e.target.value);
-          props.onChange(e);
-        }}
-        value={valor}
+  return (
+    <textarea
+      className={classNames({
+        [styles.textBox]: true,
+        [styles['nao-ajustavel']]: !ajustavel,
+        [props.className]: true
+      })}
+      placeholder={props.placeholder}
+      onChange={(e) => {
+        setValor(e.target.value);
+        props.onChange(e);
+      }}
+      value={valor}
     />
   );
 };
