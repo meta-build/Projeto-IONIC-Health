307aac0639086c927bd5ab67956a659a915b313b
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-02 11:06:27 -0300

    style: clear empty space in textBox code component

--- frontend/src/components/Inputs/TextBox/index.tsx
+++ frontend/src/components/Inputs/TextBox/index.tsx
@@ -2,7 +2,6 @@ import classNames from "classnames";
 import styles from "./TextBox.module.scss";
 import { useEffect, useState } from "react";
 
-
 interface Props {
     placeholder?:string;
     onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
@@ -36,6 +35,3 @@ export default function TextBox(props: Props) {
     />
   );
 };
-
-
-
