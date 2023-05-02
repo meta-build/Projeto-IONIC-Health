8808e43b60458f62b57c0908cc0be325587bd538
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-02 12:01:46 -0300

    style: update code indent on GooleIcon and Header component

--- frontend/src/components/Header/Header32.tsx
+++ frontend/src/components/Header/Header32.tsx
@@ -1,22 +1,20 @@
-import { ReactElement, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import styles from './Header.module.scss';
 import classNames from 'classnames';
 
 interface Props {
-    className?: string;
-    children: ReactNode;
+  className?: string;
+  children: ReactNode;
 }
 
-export function Header32 (props: Props) {
-    const {children, className = ''} = props
-
-    return(
-        <h1 className={classNames({
-            [styles.header]: true,
-            [styles.header32]: true,
-            [className]: true
-        })}>
-            {children}
-        </h1>
-    )
+export function Header32(props: Props) {
+  return (
+    <h1 className={classNames({
+      [styles.header]: true,
+      [styles.header32]: true,
+      [props.className]: true
+    })}>
+      {props.children}
+    </h1>
+  );
 }
