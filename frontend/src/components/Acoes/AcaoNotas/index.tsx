0928d4ff03873029f4e1c1385252bc15a1838417
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-02 22:10:13 -0300

    style: actions components

--- frontend/src/components/Acoes/AcaoNotas/index.tsx
+++ frontend/src/components/Acoes/AcaoNotas/index.tsx
@@ -1,49 +1,36 @@
-import {BotaoNota} from "../../../Botoes";
+import { BotaoNota } from '../../Botoes';
 import styles from './AcaoNotas.module.scss';
 
 interface NotaProps {
-    nota: number,
-    comite: string
+  nota: number,
+  comite: string
 }
 interface Props {
-    notas: NotaProps[];
-    notaPreenchida?: boolean
+  notas: NotaProps[];
+  notaPreenchida?: boolean
 }
 
-export default function AcaoNotas (props: Props) {
+export default function AcaoNotas(props: Props) {
 
-    const {notaPreenchida = false} = props;
+  const { notaPreenchida = false } = props;
 
-    const corRiscoCusto:
+  const corRiscoCusto:
     ("cinza" | "verde" | "amarelo" | "vermelho" | "azul1" | "azul2")[] =
-    [ 'cinza', 'verde', 'amarelo', 'vermelho' ]
+    ['cinza', 'verde', 'amarelo', 'vermelho'];
 
-    const corImpacto:
-    ("cinza" | "verde" | "amarelo" | "vermelho" | "azul1" | "azul2")[] =
-    [ 'cinza', 'verde', 'azul1', 'azul2' ]
-
-    return (
-        <span className={styles.container}>
-            {/* {props.notaRisco !== undefined && <span className={styles.nota}>
-                Risco:
-                <BotaoNota
-                valor={props.notaRisco}
-                cor={corRiscoCusto[props.notaRisco]}
-                selecionado={notaPreenchida}
-                className={styles.numero}
-                />
-            </span>} */}
-            {props.notas.map(nota => (
-                <span className={styles.nota}>
-                {nota.comite}:
-                <BotaoNota
-                valor={nota.nota}
-                cor={corRiscoCusto[nota.nota]}
-                selecionado={notaPreenchida}
-                className={styles.numero}
-                />
-            </span>
-            ))}
+  return (
+    <span className={styles.container}>
+      {props.notas.map(nota => (
+        <span className={styles.nota}>
+          {nota.comite}:
+          <BotaoNota
+            valor={nota.nota}
+            cor={corRiscoCusto[nota.nota]}
+            selecionado={notaPreenchida}
+            className={styles.numero}
+          />
         </span>
-    );
+      ))}
+    </span>
+  );
 }
--- frontend/src/components/Acoes/AcaoProducao/index.tsx
+++ frontend/src/components/Acoes/AcaoProducao/index.tsx
@@ -2,33 +2,33 @@ import classNames from 'classnames';
 import styles from './AcaoProducao.module.scss';
 
 interface Props {
-    status: 'new' | 'on-holding' | 'done';
+  status: 'new' | 'on-holding' | 'done';
 }
 
-export default function AcaoProducao (props: Props) {
+export default function AcaoProducao(props: Props) {
 
-    const strStatus = (status: string) => {
-        switch(status){
-            case 'new':
-                return 'New';
-            case 'on-holding':
-                return 'On Holding';
-            default:
-                return 'Done'
-        }
+  const strStatus = (status: string) => {
+    switch (status) {
+      case 'new':
+        return 'New';
+      case 'on-holding':
+        return 'On Holding';
+      default:
+        return 'Done'
     }
+  }
 
-    return (
-        <div className={styles.container}>
-            <span className={styles.label}>
-                Status:
-            </span>
-            <span className={classNames({
-                [styles.status]: true,
-                [styles[props.status]]: true
-            })}>
-                {strStatus(props.status)}
-            </span>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <span className={styles.label}>
+        Status:
+      </span>
+      <span className={classNames({
+        [styles.status]: true,
+        [styles[props.status]]: true
+      })}>
+        {strStatus(props.status)}
+      </span>
+    </div>
+  );
 }
