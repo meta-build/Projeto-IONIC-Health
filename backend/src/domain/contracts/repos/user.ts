ea7352e6832795b0ea5baec255d2f420fdbc0341
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-03 14:03:53 -0300

    feat: add LoadUserByEmail interface

--- backend/src/domain/contracts/repos/user.ts
+++ backend/src/domain/contracts/repos/user.ts
@@ -1,13 +1,30 @@
-export interface DbCreateUser {
-  create: (input: DbCreateUser.Input) => Promise<DbCreateUser.Output>
+export interface CreateUser {
+  create: (input: CreateUser.Input) => Promise<CreateUser.Output>
 }
 
-export namespace DbCreateUser {
+export namespace CreateUser {
   export type Input = {
     name: string
     email: string
     password: string
     roleId: number
+  }
+  export type Output = {
+    id: number
+    name: string
+    email: string
+    password: string
+    roleId: number
+  }
+}
+
+export interface LoadUserByEmail {
+  loadByEmail: (input: LoadUserByEmail.Input) => Promise<LoadUserByEmail.Output>
+}
+
+export namespace LoadUserByEmail {
+  export type Input = {
+    email: string
   }
   export type Output = {
     id: number
