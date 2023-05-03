ab115a651eee8c86e0dd747cf0eb03a48ee99f8b
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-03 14:16:40 -0300

    feat: ensure login route use LoginController

--- backend/src/main/routes/user-routes.ts
+++ backend/src/main/routes/user-routes.ts
@@ -1,13 +1,13 @@
 import userController from '@/application/controllers/user/user-controller'
 import { authorization } from '@/main/middlewares'
-import { makeCreateUserController } from '@/main/factories/application/controllers/'
+import { makeCreateUserController, makeLoginController } from '@/main/factories/application/controllers/'
 import { adaptRoute } from '../adapters'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateUserController()))
-  router.post('/login', userController.login)
+  router.post('/login', adaptRoute(makeLoginController()))
   router.post('/user', userController.create)
   router.put('/user/:id', authorization, userController.update)
   router.get('/user/:id', authorization, userController.getUserById)
