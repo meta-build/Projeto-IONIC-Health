6ceb8c66853f6c3a2aa124d2cf2639ad99234306
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-03 14:17:08 -0300

    refactor: remove unused route

--- backend/src/main/routes/user-routes.ts
+++ backend/src/main/routes/user-routes.ts
@@ -8,7 +8,6 @@ import { Router } from 'express'
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateUserController()))
   router.post('/login', adaptRoute(makeLoginController()))
-  router.post('/user', userController.create)
   router.put('/user/:id', authorization, userController.update)
   router.get('/user/:id', authorization, userController.getUserById)
   router.get('/user', authorization, userController.getAllUser)
