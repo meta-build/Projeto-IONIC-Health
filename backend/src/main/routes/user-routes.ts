c29800e38742b2e4c52655e0c628748e4ce54745
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-03 15:04:15 -0300

    feat: ensure get user route uses the new middleware

--- backend/src/main/routes/user-routes.ts
+++ backend/src/main/routes/user-routes.ts
@@ -1,7 +1,8 @@
 import userController from '@/application/controllers/user/user-controller'
 import { authorization } from '@/main/middlewares'
 import { makeCreateUserController, makeLoginController } from '@/main/factories/application/controllers/'
-import { adaptRoute } from '../adapters'
+import { makeAuthMiddleware } from '@/main/factories/middlewares'
+import { adaptMiddleware, adaptRoute } from '../adapters'
 
 import { Router } from 'express'
 
@@ -9,7 +10,7 @@ export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateUserController()))
   router.post('/login', adaptRoute(makeLoginController()))
   router.put('/user/:id', authorization, userController.update)
-  router.get('/user/:id', authorization, userController.getUserById)
+  router.get('/user/:id', adaptMiddleware(makeAuthMiddleware()), userController.getUserById)
   router.get('/user', authorization, userController.getAllUser)
   router.delete('/user/:id', authorization, userController.deleteUser)
 }
