9abec2abae5a4c9bb14c9aaa3dbd55a6c96e596a
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-10 17:52:39 -0300

    refactor: update PermissionRepository getAllById to query correctly

--- backend/src/infra/repositories/mysql/permission-repository.ts
+++ backend/src/infra/repositories/mysql/permission-repository.ts
@@ -40,12 +40,25 @@ export class PermissionRepository
     input: GetPermissionsById.Input
   ): Promise<GetPermissionsById.Output> {
     const permissionRepo = this.getRepository(Permission)
+    let permissions: Permission[]
 
-    const permissions = permissionRepo
-      .createQueryBuilder('permissions')
-      .where('permission.id IN (:...ids)', { ids: input.ids })
-      .getMany()
+    if (input?.ids) {
+      permissions = await permissionRepo.query(
+        'SELECT * FROM permission WHERE id IN (?)',
+        [[input.ids]]
+      )
+    }
 
-    return permissions
+    if (permissions) {
+      return permissions.map((permission: Permission) => ({
+        id: permission.id,
+        permissionName: permission.permissionName,
+        humanizedPermissionName: permission.humanizedPermissionName,
+        entity: permission.entity,
+        humanizedEntity: permission.humanizedEntity
+      }))
+    }
+
+    return []
   }
 }
