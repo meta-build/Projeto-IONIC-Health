2a6c1492c28bf880c1f30b1dad2c177739107e04
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-10 19:54:52 -0300

    feat: add CreateUser permission to user route

--- backend/src/main/routes/user-routes.ts
+++ backend/src/main/routes/user-routes.ts
@@ -1,16 +1,27 @@
 import userController from '@/application/controllers/user/user-controller'
 import { authorization } from '@/main/middlewares'
-import { makeCreateUserController, makeLoginController } from '@/main/factories/application/controllers/'
+import {
+  makeCreateUserController,
+  makeLoginController
+} from '@/main/factories/application/controllers/'
 import { makeAuthMiddleware } from '@/main/factories/middlewares'
 import { adaptMiddleware, adaptRoute } from '../adapters'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeCreateUserController()))
   router.post('/login', adaptRoute(makeLoginController()))
+  router.post(
+    '/user',
+    adaptMiddleware(makeAuthMiddleware(['CreateUser'])),
+    adaptRoute(makeCreateUserController())
+  )
   router.put('/user/:id', authorization, userController.update)
-  router.get('/user/:id', adaptMiddleware(makeAuthMiddleware()), userController.getUserById)
+  router.get(
+    '/user/:id',
+    adaptMiddleware(makeAuthMiddleware()),
+    userController.getUserById
+  )
   router.get('/user', authorization, userController.getAllUser)
   router.delete('/user/:id', authorization, userController.deleteUser)
 }
