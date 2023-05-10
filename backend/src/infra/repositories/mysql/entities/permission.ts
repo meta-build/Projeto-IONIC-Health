d47426830cd1390cec369b10b2966215150061d4
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-10 14:29:28 -0300

    feat: add Permission entity

---
+++ backend/src/infra/repositories/mysql/entities/permission.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm'
+import { Role } from './role'
+
+@Entity({ name: 'permission' })
+export class Permission {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ nullable: false })
+  permissionName: string
+
+  @Column({ nullable: false })
+  humanizedPermissionName: string
+
+  @Column({ nullable: false })
+  entity: string
+
+  @Column({ nullable: false })
+  humanizedEntity: string
+
+  @ManyToMany(() => Role)
+  roles: Role[]
+}
--- backend/src/infra/repositories/mysql/entities/role.ts
+++ backend/src/infra/repositories/mysql/entities/role.ts
@@ -1,14 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { User } from "./user";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToMany
+} from 'typeorm'
+import { User } from './user'
+import { Permission } from './permission'
 
-@Entity({ name: "role" })
+@Entity({ name: 'role' })
 export class Role {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Column({ nullable: false, unique: true, length: 100 })
-  name: string;
+  name: string
 
-  @OneToMany(() => User, user => user.role)
-  users: User[];
+  @Column({ nullable: false, default: false })
+  isAdmin: boolean
+
+  @OneToMany(() => User, (user) => user.role)
+  users: User[]
+
+  @ManyToMany(() => Permission)
+  permissions: Permission[]
 }
