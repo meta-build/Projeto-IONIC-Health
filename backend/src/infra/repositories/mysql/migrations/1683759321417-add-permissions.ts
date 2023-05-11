4a24a1627a3b501a25f0d16ae9a328e0a1662975
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-11 11:00:50 -0300

    feat: add permissions migration

---
+++ backend/src/infra/repositories/mysql/migrations/1683759321417-add-permissions.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class Default1683759321417 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+			INSERT INTO permission (permissionName, humanizedPermissionName, entity, humanizedEntity)
+			VALUES
+			('CreateUser', 'Criar Usuário', 'User', 'Usuários'),
+			('UpdateUser', 'Editar Usuário', 'User', 'Usuários'),
+			('DeleteUser', 'Excluir Usuário', 'User', 'Usuários'),
+			('CreateRole', 'Criar Grupo', 'Role', 'Grupos'),
+			('UpdateRole', 'Editar Grupo', 'Role', 'Grupos'),
+			('DeleteRole', 'Excluir Grupo', 'Role', 'Grupos'),
+			('CreateTicket', 'Criar Solicitação', 'Ticket', 'Solicitações'),
+			('UpdateTicket', 'Editar Solicitação', 'Ticket', 'Solicitações'),
+			('DeleteTicket', 'Excluir Solicitação', 'Ticket', 'Solicitações'),
+			('ArchiveTicket', 'Arquivar Solicitação', 'Ticket', 'Solicitações'),
+			('ApproveTicketToProd', 'Aprovar solicitação para produção', 'Ticket', 'Solicitações'),
+			('ApproveTicketToRating', 'Liberar solicitação para avaliação', 'Ticket', 'Solicitações'),
+			('UpdateTicketProd', 'Alterar status de produção da solicitação', 'Ticket', 'Solicitações'),
+			('CreateRiksRating', 'Avaliar solicitação para Risco', 'Rating', 'Avaliações'),
+			('CreateImpactRating', 'Avaliar solicitação para Impacto', 'Rating', 'Avaliações'),
+			('CreateCostRating', 'Avaliar solicitação para Custo', 'Rating', 'Avaliações')
+		`)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+			DELETE FROM permission
+			WHERE permissionName IN (
+				'CreateUser',
+				'UpdateUser',
+				'DeleteUser',
+				'CreateRole',
+				'UpdateRole',
+				'DeleteRole',
+				'CreateTicket',
+				'UpdateTicket',
+				'DeleteTicket',
+				'ArchiveTicket',
+				'ApproveTicketToProd',
+				'ApproveTicketToRating',
+				'UpdateTicketProd',
+				'CreateRiksRating',
+				'CreateImpactRating',
+				'CreateCostRating'
+			)
+		`)
+  }
+}
