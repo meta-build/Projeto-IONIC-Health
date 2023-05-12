2a2f08073a1d92b96b806702084471a1290fb07d
Rafael Pinto Cajé <rafa_pc_sjc@hotmail.com>
2023-05-12 01:40:54 -0300

    Update backend/src/infra/repositories/mysql/ticket-repository.ts
    lint - tab - verificar create
    
    Co-authored-by: Gabriel Fabian <61850709+gabriel-fabian@users.noreply.github.com>

--- backend/src/infra/repositories/mysql/ticket-repository.ts
+++ backend/src/infra/repositories/mysql/ticket-repository.ts
@@ -1,47 +1,51 @@
 import { Ticket } from './entities'
 import DataSource from './data-source'
-import { CreateTicket, LoadTicketByReqId } from '@/domain/contracts/repos/ticket'
-  
+import {
+  CreateTicket,
+  LoadTicketByReqId
+} from '@/domain/contracts/repos/ticket'
+
 import { ObjectType, Repository } from 'typeorm'
 
-  
-export class TicketRepository
-    implements CreateTicket, LoadTicketByReqId
-{
-getRepository(entity: ObjectType<Ticket>): Repository<Ticket> {
+export class TicketRepository implements CreateTicket, LoadTicketByReqId {
+  getRepository(entity: ObjectType<Ticket>): Repository<Ticket> {
     return DataSource.getRepository(entity)
-}
+  }
 
-async create({
+  async create({
     title,
     type,
     description
-}: CreateTicket.Input): Promise<CreateTicket.Output> {
+  }: CreateTicket.Input): Promise<CreateTicket.Output> {
     const ticketRepo = this.getRepository(Ticket)
     const ticket = ticketRepo.create({
-        title,
-        type,
-        description
+      title,
+      type,
+      description
     })
 
     await ticketRepo.save(ticket)
 
     return {
-        id: ticket.id,
-        title: ticket.title,
-        type: ticket.type,
-        description: ticket.description
+      id: ticket.id,
+      title: ticket.title,
+      type: ticket.type,
+      description: ticket.description
     }
-}
-async loadTicketById({ id }: LoadTicketByReqId.Input): Promise<LoadTicketByReqId.Output> {
+  }
+
+  async loadTicketById({
+    id
+  }: LoadTicketByReqId.Input): Promise<LoadTicketByReqId.Output> {
     const ticketRepo = this.getRepository(Ticket)
 
     const ticket = await ticketRepo.findOneBy({ id })
 
     if (ticket) {
-        return ticket
+      return ticket
     }
 
     return null
-    }
+  }
 }
+
