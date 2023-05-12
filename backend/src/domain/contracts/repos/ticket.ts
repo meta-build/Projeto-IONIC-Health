b316537d7f9a46ce3ba031fe562e5ef68c26b0f3
Rafael Pinto Cajé <rafa_pc_sjc@hotmail.com>
2023-05-12 01:31:49 -0300

    Update backend/src/domain/contracts/repos/ticket.ts
    linha final e identação
    
    Co-authored-by: Gabriel Fabian <61850709+gabriel-fabian@users.noreply.github.com>

--- backend/src/domain/contracts/repos/ticket.ts
+++ backend/src/domain/contracts/repos/ticket.ts
@@ -1,33 +1,35 @@
 export interface CreateTicket {
-    create: (input: CreateTicket.Input) => Promise<CreateTicket.Output>
-  }
-  
+  create: (input: CreateTicket.Input) => Promise<CreateTicket.Output>
+}
+
 export namespace CreateTicket {
-    export type Input = {
-        title: string,
-        type: string,
-        description: string
-    }
-    export type Output = {
-        id: number,
-        title: string,
-        type: string,
-        description: string
-    }
+  export type Input = {
+    title: string
+    type: string
+    description: string
+  }
+  export type Output = {
+    id: number
+    title: string
+    type: string
+    description: string
+  }
 }
 
 export interface LoadTicketByReqId {
-    loadTicketById: (input: LoadTicketByReqId.Input) => Promise<LoadTicketByReqId.Output>
+  loadTicketById: (
+    input: LoadTicketByReqId.Input
+  ) => Promise<LoadTicketByReqId.Output>
 }
-    
+
 export namespace LoadTicketByReqId {
-    export type Input = {
-        id: number
-    }
-    export type Output = {
-        id: number,
-        title: string,
-        type: string,
-        description: string
-    }
+  export type Input = {
+    id: number
+  }
+  export type Output = {
+    id: number
+    title: string
+    type: string
+    description: string
+  }
 }
