adc3b1a0c536a4123858b4f56000680a8eb3f16a
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-12 15:37:26 -0300

    refactor!: update existing migration permission-roles-join

--- backend/src/infra/repositories/mysql/migrations/1683751693951-add-permission-roles-join.ts
+++ backend/src/infra/repositories/mysql/migrations/1683751693951-add-permission-roles-join.ts
@@ -4,25 +4,17 @@ export class AddPermissionRolesJoin1683751693951 implements MigrationInterface {
     name = 'AddPermissionRolesJoin1683751693951'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`permission_roles_role\` (\`permissionId\` int NOT NULL, \`roleId\` int NOT NULL, INDEX \`IDX_9f44b6228b173c7b9dfb8c6600\` (\`permissionId\`), INDEX \`IDX_7ec93d4fbf75b063f3ffd2646a\` (\`roleId\`), PRIMARY KEY (\`permissionId\`, \`roleId\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`role_permissions_permission\` (\`roleId\` int NOT NULL, \`permissionId\` int NOT NULL, INDEX \`IDX_b36cb2e04bc353ca4ede00d87b\` (\`roleId\`), INDEX \`IDX_bfbc9e263d4cea6d7a8c9eb3ad\` (\`permissionId\`), PRIMARY KEY (\`roleId\`, \`permissionId\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`permission_roles_role\` ADD CONSTRAINT \`FK_9f44b6228b173c7b9dfb8c66003\` FOREIGN KEY (\`permissionId\`) REFERENCES \`permission\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE \`permission_roles_role\` ADD CONSTRAINT \`FK_7ec93d4fbf75b063f3ffd2646a5\` FOREIGN KEY (\`roleId\`) REFERENCES \`role\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE \`role_permissions_permission\` ADD CONSTRAINT \`FK_b36cb2e04bc353ca4ede00d87b9\` FOREIGN KEY (\`roleId\`) REFERENCES \`role\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE \`role_permissions_permission\` ADD CONSTRAINT \`FK_bfbc9e263d4cea6d7a8c9eb3ad2\` FOREIGN KEY (\`permissionId\`) REFERENCES \`permission\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`CREATE TABLE \`role_permission\` (\`permissionId\` int NOT NULL, \`roleId\` int NOT NULL, INDEX \`IDX_72e80be86cab0e93e67ed1a7a9\` (\`permissionId\`), INDEX \`IDX_e3130a39c1e4a740d044e68573\` (\`roleId\`), PRIMARY KEY (\`permissionId\`, \`roleId\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`role_permission\` ADD CONSTRAINT \`FK_72e80be86cab0e93e67ed1a7a9a\` FOREIGN KEY (\`permissionId\`) REFERENCES \`permission\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
+        await queryRunner.query(`ALTER TABLE \`role_permission\` ADD CONSTRAINT \`FK_e3130a39c1e4a740d044e685730\` FOREIGN KEY (\`roleId\`) REFERENCES \`role\`(\`id\`) ON DELETE CASCADE ON UPDATE CASCADE`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`role_permissions_permission\` DROP FOREIGN KEY \`FK_bfbc9e263d4cea6d7a8c9eb3ad2\``);
-        await queryRunner.query(`ALTER TABLE \`role_permissions_permission\` DROP FOREIGN KEY \`FK_b36cb2e04bc353ca4ede00d87b9\``);
-        await queryRunner.query(`ALTER TABLE \`permission_roles_role\` DROP FOREIGN KEY \`FK_7ec93d4fbf75b063f3ffd2646a5\``);
-        await queryRunner.query(`ALTER TABLE \`permission_roles_role\` DROP FOREIGN KEY \`FK_9f44b6228b173c7b9dfb8c66003\``);
-        await queryRunner.query(`DROP INDEX \`IDX_bfbc9e263d4cea6d7a8c9eb3ad\` ON \`role_permissions_permission\``);
-        await queryRunner.query(`DROP INDEX \`IDX_b36cb2e04bc353ca4ede00d87b\` ON \`role_permissions_permission\``);
-        await queryRunner.query(`DROP TABLE \`role_permissions_permission\``);
-        await queryRunner.query(`DROP INDEX \`IDX_7ec93d4fbf75b063f3ffd2646a\` ON \`permission_roles_role\``);
-        await queryRunner.query(`DROP INDEX \`IDX_9f44b6228b173c7b9dfb8c6600\` ON \`permission_roles_role\``);
-        await queryRunner.query(`DROP TABLE \`permission_roles_role\``);
+        await queryRunner.query(`ALTER TABLE \`role_permission\` DROP FOREIGN KEY \`FK_e3130a39c1e4a740d044e685730\``);
+        await queryRunner.query(`ALTER TABLE \`role_permission\` DROP FOREIGN KEY \`FK_72e80be86cab0e93e67ed1a7a9a\``);
+        await queryRunner.query(`DROP INDEX \`IDX_e3130a39c1e4a740d044e68573\` ON \`role_permission\``);
+        await queryRunner.query(`DROP INDEX \`IDX_72e80be86cab0e93e67ed1a7a9\` ON \`role_permission\``);
+        await queryRunner.query(`DROP TABLE \`role_permission\``);
     }
 
 }
