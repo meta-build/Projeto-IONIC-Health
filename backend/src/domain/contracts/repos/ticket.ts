db51fbca4e36a70f1ec01e7890cc44d1a265dfd9
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-13 19:06:34 -0300

    feat: add UpdateTicket interface

--- backend/src/domain/contracts/repos/ticket.ts
+++ backend/src/domain/contracts/repos/ticket.ts
@@ -1,6 +1,29 @@
 import { Attachment } from '@/infra/repositories/mysql/entities/attachment'
 import { Rating } from '@/infra/repositories/mysql/entities/rating'
 
+export interface UpdateTicket {
+  update: (input: UpdateTicket.Input) => Promise<UpdateTicket.Output>
+}
+
+export namespace UpdateTicket {
+  export type Input = {
+    id: number,
+    title?: string,
+    description?: string,
+    status?: string,
+    updatedAt?: Date,
+    archivedAt?: Date
+  }
+  export type Output = {
+    id: number
+    title: string
+    requesterId: number
+    type: string
+    description: string
+    status: string
+  }
+}
+
 export interface LoadTicketById {
   loadById: (input: LoadTicketById.Input) => Promise<LoadTicketById.Output>
 }
