1f7ceb6ed3934de1dbaad6f16e3009ce6dcc7afc
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-13 17:19:53 -0300

    refactor: update variable name

--- backend/src/main/factories/application/controllers/user/get-user-by-id-controller-factory.ts
+++ backend/src/main/factories/application/controllers/user/get-user-by-id-controller-factory.ts
@@ -2,6 +2,6 @@ import { Controller, GetUserByIdController } from '@/application/controllers'
 import { UserRepository } from '@/infra/repositories/mysql/user-repository'
 
 export const makeGetUserByIdController = (): Controller => {
-  const ticketRepository = new UserRepository()
-  return new GetUserByIdController(ticketRepository)
+  const userRepository = new UserRepository()
+  return new GetUserByIdController(userRepository)
 }
