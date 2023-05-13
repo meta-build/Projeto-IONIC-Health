b267dca7e5d9ba0647956ac713d471707907f921
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-12 21:09:28 -0300

    feat: add RoleRepository update and loadById

--- backend/src/infra/repositories/mysql/role-repository.ts
+++ backend/src/infra/repositories/mysql/role-repository.ts
@@ -1,12 +1,10 @@
-import { CreateRole} from '@/domain/contracts/repos'
+import { CreateRole, LoadRoleById, UpdateRole } from '@/domain/contracts/repos'
 import { Role } from './entities'
 import DataSource from './data-source'
 
 import { ObjectType, Repository } from 'typeorm'
 
-export class RoleRepository
-  implements CreateRole
-{
+export class RoleRepository implements CreateRole, UpdateRole, LoadRoleById {
   getRepository(entity: ObjectType<Role>): Repository<Role> {
     return DataSource.getRepository(entity)
   }
@@ -29,7 +27,45 @@ export class RoleRepository
       id: role.id,
       name: role.name,
       isAdmin: role.isAdmin,
-      permissions: role.permissions,
+      permissions: role.permissions
     }
+  }
+
+  async update({
+    id,
+    name,
+    isAdmin,
+    permissions
+  }: UpdateRole.Input): Promise<UpdateRole.Output> {
+    const roleRepository = this.getRepository(Role)
+    const role = await roleRepository.findOneBy({ id })
+
+    role.name = name ?? role.name
+    role.isAdmin = isAdmin ?? role.isAdmin
+    role.permissions = permissions ?? role.permissions
+
+    await roleRepository.save(role)
+
+    return {
+      id: role.id,
+      name: role.name,
+      isAdmin: role.isAdmin,
+      permissions: role.permissions
+    }
+  }
+
+  async loadById({ id }: LoadRoleById.Input): Promise<LoadRoleById.Output> {
+    const roleRepository = this.getRepository(Role)
+    const role = await roleRepository
+      .createQueryBuilder('role')
+      .leftJoinAndSelect('role.permissions', 'permission')
+      .where('role.id = :id', { id })
+      .getOne()
+
+    if (!role) {
+      return null
+    }
+
+    return role
   }
 }
