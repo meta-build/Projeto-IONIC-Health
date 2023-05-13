ed34808016b0e8443958ca794d47fdc53c9299aa
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-12 21:08:38 -0300

    feat: add params to controller request

--- backend/src/main/adapters/express-router.ts
+++ backend/src/main/adapters/express-router.ts
@@ -4,8 +4,13 @@ import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
-    // O res.locals deverá ser removido ao fim da refatoração.
-    const httpResponse = await controller.handle({ ...req.body, ...req.locals, ...res.locals, fileDataList: req.fileDataList })
+    const httpResponse = await controller.handle({
+      ...req.body,
+      ...req.locals,
+      ...res.locals,
+      fileDataList: req.fileDataList,
+      params: req.params
+    })
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.data)
     } else {
