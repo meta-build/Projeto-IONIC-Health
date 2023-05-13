7c93219e9ccc8fc19f80fdd0aca1c81df93a160e
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-12 21:04:55 -0300

    feat: add UpdateRole and LoadRoleById contract

--- backend/src/domain/contracts/repos/role.ts
+++ backend/src/domain/contracts/repos/role.ts
@@ -1,3 +1,5 @@
+import { Role } from '@/infra/repositories/mysql/entities'
+
 export interface CreateRole {
   create: (input: CreateRole.Input) => Promise<CreateRole.Output>
 }
@@ -13,6 +15,60 @@ export namespace CreateRole {
       entity: string
       humanizedEntity: string
     }>
+  }
+  export type Output = {
+    id: number
+    name: string
+    isAdmin: boolean
+    permissions: Array<{
+      id: number
+      permissionName: string
+      humanizedPermissionName: string
+      entity: string
+      humanizedEntity: string
+    }>
+  }
+}
+
+export interface UpdateRole {
+  update: (input: UpdateRole.Input) => Promise<UpdateRole.Output>
+}
+
+export namespace UpdateRole {
+  export type Input = {
+    id: number
+    name: string
+    isAdmin: boolean
+    permissions?: Array<{
+      id: number
+      permissionName: string
+      humanizedPermissionName: string
+      entity: string
+      humanizedEntity: string
+      roles: Role[]
+    }>
+  }
+  export type Output = {
+    id: number
+    name: string
+    isAdmin: boolean
+    permissions: Array<{
+      id: number
+      permissionName: string
+      humanizedPermissionName: string
+      entity: string
+      humanizedEntity: string
+    }>
+  }
+}
+
+export interface LoadRoleById {
+  loadById: (input: LoadRoleById.Input) => Promise<LoadRoleById.Output>
+}
+
+export namespace LoadRoleById {
+  export type Input = {
+    id: number
   }
   export type Output = {
     id: number
