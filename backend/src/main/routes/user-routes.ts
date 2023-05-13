b84d468342f345e9e07a4cacad27ebb2f496a289
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-13 17:01:27 -0300

    feat: update user get by id route to use new controller

--- backend/src/main/routes/user-routes.ts
+++ backend/src/main/routes/user-routes.ts
@@ -1,17 +1,29 @@
 import userController from '@/application/controllers/user/user-controller'
 import { authorization } from '@/main/middlewares'
-import { makeCreateUserController, makeLoginController } from '@/main/factories/application/controllers/'
+import {
+  makeCreateUserController,
+  makeLoginController,
+  makeUpdateUserController,
+  makeGetUserByIdController
+} from '@/main/factories/application/controllers/'
 import { makeAuthMiddleware } from '@/main/factories/middlewares'
-import { adaptMiddleware, adaptRoute } from '../adapters'
+import { adaptMiddleware, adaptRoute } from '@/main/adapters'
 
 import { Router } from 'express'
-import { makeUpdateUserController } from '../factories/application/controllers/user/update-user-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeCreateUserController()))
   router.post('/login', adaptRoute(makeLoginController()))
-  router.put('/user/:id', adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeUpdateUserController()))
-  router.get('/user/:id', adaptMiddleware(makeAuthMiddleware()), userController.getUserById)
+  router.put(
+    '/user/:id',
+    adaptMiddleware(makeAuthMiddleware()),
+    adaptRoute(makeUpdateUserController())
+  )
+  router.get(
+    '/user/:id',
+    adaptMiddleware(makeAuthMiddleware()),
+    adaptRoute(makeGetUserByIdController())
+  )
   router.get('/user', authorization, userController.getAllUser)
   router.delete('/user/:id', authorization, userController.deleteUser)
 }
