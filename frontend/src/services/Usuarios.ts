1ed95e1f37ff837fbd2ea37d6dab0fd04b3b49af
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-15 09:09:33 -0300

    fix: update api url

--- frontend/src/services/Usuarios.ts
+++ frontend/src/services/Usuarios.ts
@@ -15,27 +15,27 @@ interface Login {
 
 class Usuarios {
   async criar(usuario: Usuario) {
-    const { data } = await api.post('/api/user', usuario);
+    const { data } = await api.post('user', usuario);
     return data;
   }
 
   async editar(id: number, usuarioAtualizado: EditarUsuarioProps) {
-    const { data } = await api.put(`api/user/${id}`, usuarioAtualizado);
+    const { data } = await api.put(`user/${id}`, usuarioAtualizado);
     return data;
   }
 
   async deletar(id: number) {
-    const { data } = await api.delete(`api/user/${id}`)
+    const { data } = await api.delete(`user/${id}`)
     return data;
   }
 
   async getByID(id: number): Promise<UsuarioProps> {
-    const { data } = await api.get(`api/user/${id}`)
+    const { data } = await api.get(`user/${id}`)
     return data;
   }
 
   async getAll(): Promise<UsuarioProps[]> {
-    const { data } = await api.get('api/user')
+    const { data } = await api.get('user')
     return data;
   }
 
