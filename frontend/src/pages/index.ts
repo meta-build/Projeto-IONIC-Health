755dac016f0725943d8488ca3f1a1c1201d2e04b
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-16 11:41:27 -0300

    refactor: clear unused pages and popups

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,26 +1,17 @@
 import Home from "./Home";
-import HomeAvaliador from "./HomeAvaliador";
-import HomeSolicitante from "./HomeSolicitante";
 import Tests from "./Tests";
 import Login from "./Login";
 import PaginaNaoEncontrada from "./PaginaNaoEncontrada";
-import SolicitacoesAdm from "./SolicitacoesAdm";
-import UsuariosAdm from "./UsuariosAdm";
 import CriarGrupo from "./CriarGrupo";
 import CriarSolicitacao from "./CriarSolicitacao";
-
 import ListaSolicitacoes from "./ListaSolicitacoes";
 import ListaUsuarios from "./ListaUsuarios";
 import ListGrupos from "./ListGrupos";
 
 export {
   Home,
-  HomeAvaliador,
-  HomeSolicitante,
   Login,
   PaginaNaoEncontrada,
-  SolicitacoesAdm,
-  UsuariosAdm,
   Tests,
   ListaSolicitacoes,
   ListaUsuarios,
