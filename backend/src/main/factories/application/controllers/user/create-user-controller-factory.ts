b3357a563b49ba963c8744172ae8316be9869616
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-27 09:32:36 -0300

    feat: ensure CreateUserController factory uses PermissionRepository

--- backend/src/main/factories/application/controllers/user/create-user-controller-factory.ts
+++ backend/src/main/factories/application/controllers/user/create-user-controller-factory.ts
@@ -1,5 +1,6 @@
 import { Controller, CreateUserController } from '@/application/controllers'
 import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter'
+import { PermissionRepository } from '@/infra/repositories/mysql/permission-repository'
 import { UserRepository } from '@/infra/repositories/mysql/user-repository'
 import { makeCreateUserValidation } from './create-user-validation'
 
@@ -7,5 +8,6 @@ export const makeCreateUserController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const userRepository = new UserRepository()
-  return new CreateUserController(makeCreateUserValidation(), bcryptAdapter, userRepository)
+  const permissionRepository = new PermissionRepository()
+  return new CreateUserController(makeCreateUserValidation(), bcryptAdapter, userRepository, permissionRepository)
 }
