739faa2f0f7edc6120af625c51c631e32a86d40c
Gabriel Fabian <gabrielferreirafabian@gmail.com>
2023-05-27 13:45:18 -0300

    feat: ensure LocalFileStorage implements DeleteFile

--- backend/src/infra/storage/local-file-storage.ts
+++ backend/src/infra/storage/local-file-storage.ts
@@ -1,10 +1,10 @@
-import { SaveFile } from '../../domain/contracts'
+import { DeleteFile, SaveFile } from '../../domain/contracts'
 import { FileData } from '../../domain/entities'
 
 import path from 'path'
 import fs from 'fs'
 
-export class LocalFileStorage implements SaveFile {
+export class LocalFileStorage implements SaveFile, DeleteFile {
   private readonly storagePath = path.join(__dirname, '../../../', 'uploads')
   type = 'local'
 
@@ -22,5 +22,25 @@ export class LocalFileStorage implements SaveFile {
     await fs.promises.writeFile(filePath, buffer)
 
     return `/uploads/${fileName}`
+  }
+
+  async deleteFile(fileUrl: string): Promise<void> {
+    const fileName = path.basename(fileUrl);
+    const filePath = path.join(this.storagePath, fileName);
+
+    try {
+      await new Promise<void>((resolve, reject) => {
+        fs.unlink(filePath, (err) => {
+          if (err) {
+            console.error('Erro ao excluir o arquivo:', err);
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 }
