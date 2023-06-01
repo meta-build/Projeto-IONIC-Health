1ded5e0745210562823c20a3f134c01fadc1f200
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-06-01 10:37:38 -0300

    feat: update ticket status view

---
+++ frontend/src/pages/ListaSolicitacoes/SolicStatusProducao/StatusProducao/index.tsx
@@ -0,0 +1,54 @@
+import classNames from "classnames";
+import styles from './StatusProducao.module.scss';
+import New from '../../../../assets/new.png';
+import Onholding from '../../../../assets/onholding.png';
+import Done from '../../../../assets/done.png';
+
+interface Props {
+  type: "ACTIVE" | "ALREADY" | "DESACTIVE";
+  status: "NEW" | "ONHOLDING" | "DONE";
+}
+
+export default function StatusProducao(props: Props) {
+  const padronizeStatus = (status: "NEW" | "ONHOLDING" | "DONE") => {
+    switch (status) {
+      case 'NEW': return "New";
+      case 'ONHOLDING': return "On Holding";
+      case 'DONE': return "Done";
+    }
+  }
+
+  const getIcon = () => {
+    switch(props.status) {
+      case "NEW": return New;
+      case "ONHOLDING": return Onholding
+      case "DONE": return Done;
+    }
+  }
+
+  return (
+    <div className={classNames({
+      [styles['solic-status']]: true,
+      [styles[props.status.toLowerCase()]]: true,
+      [styles[props.type.toLowerCase()]]: true
+    })}>
+      <img src={getIcon()} className={styles['status-icon']} />
+      <div className={styles['solic-status-info']}>
+        <div className={styles['solic-status-title']}>
+          {padronizeStatus(props.status)}
+        </div>
+        {props.type !== 'DESACTIVE' &&
+          <div className={styles['solic-status-subtitle']}>
+            {new Date().toLocaleDateString('pt-br', {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+              hour12: false
+            })}
+          </div>}
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/ListaSolicitacoes/index.tsx
+++ frontend/src/pages/ListaSolicitacoes/index.tsx
@@ -19,6 +19,7 @@ import PopupErro from '../../popUps/PopupErro';
 import PopupAprovacao from '../../popUps/PopupAprovacao';
 import { AlterarStatusProducao, AprovarParaProducao, AvaliarSolicitacao } from '../../popUps';
 import Grupos from '../../services/Grupos';
+import SolicStatusProducao from './SolicStatusProducao';
 
 export default function ListaSolicitacoes() {
   const nav = useNavigate();
@@ -205,7 +206,7 @@ export default function ListaSolicitacoes() {
                 {solicSelecionada.title}
               </h2>
               <BadgeStatus status={solicSelecionada.isArchived ? 'ARCHIVED' : solicSelecionada.status} />
-              {!solicSelecionada.isArchived && (solicSelecionada.status == 'NEW' || solicSelecionada.status == 'ONHOLDING' || solicSelecionada.status == 'DONE') &&
+              {/* {!solicSelecionada.isArchived && (solicSelecionada.status == 'NEW' || solicSelecionada.status == 'ONHOLDING' || solicSelecionada.status == 'DONE') &&
                 <div className={styles['solic-info']}>
                   <span className={styles.content}>
                     <span>{'Status: '}</span>
@@ -225,7 +226,7 @@ export default function ListaSolicitacoes() {
                       {grupoSolic ? grupoSolic.name : 'Carregando...'}
                     </span>
                   </span>
-                </div>}
+                </div>} */}
               {!solicSelecionada.isArchived && solicSelecionada.status == 'RATING' &&
                 <span className={styles['solic-em-av']}>
                   {solicSelecionada.ratings.length ?
@@ -307,6 +308,9 @@ export default function ListaSolicitacoes() {
                     ))}
                 </span>
               </div>
+              {!solicSelecionada.isArchived && (solicSelecionada.status == 'NEW' || solicSelecionada.status == 'ONHOLDING' || solicSelecionada.status == 'DONE') &&
+                <SolicStatusProducao solic={solicSelecionada} />
+                }
               <div className={styles['solic-espacador']}></div>
               <div className={styles['solic-botoes']}>
                 {!solicSelecionada.isArchived && solicSelecionada.status == 'RECENT' &&
@@ -549,7 +553,7 @@ export default function ListaSolicitacoes() {
             titulo='Erro ao liberar para avaliação'
             descricao='Não foi possível liberar para avaliação por conta de um erro interno do servidor, tente novamente mais tarde.'
           />
-            <AprovarParaProducao
+          <AprovarParaProducao
             aberto={confirmLiberarProd}
             onClose={() => {
               setConfirmLiberarProd(false);
@@ -557,7 +561,7 @@ export default function ListaSolicitacoes() {
             }}
             idSolic={solicSelecionada.id}
             onConfirm={() => console.log()}
-            />
+          />
           <PopupConfirm
             visivel={sucessoLiberarProd}
             onClose={() => {
@@ -586,13 +590,13 @@ export default function ListaSolicitacoes() {
             idSolic={solicSelecionada.id}
           />
           <AlterarStatusProducao
-          aberto={alterarProd}
-          idSolic={solicSelecionada.id}
-          onClose={() => {
-            setAlterarProd(false);
-            setSolicSelecionada(undefined);
-            getSolicitacoes();
-          }}
+            aberto={alterarProd}
+            idSolic={solicSelecionada.id}
+            onClose={() => {
+              setAlterarProd(false);
+              setSolicSelecionada(undefined);
+              getSolicitacoes();
+            }}
           />
         </>
       }
